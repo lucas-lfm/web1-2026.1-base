1a0bd187098744131a7fc30e4cd85415a3044854
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2026-05-04 20:22:25 -0300

    feat: atualiza exemplos de leitura de arquivos para usar caminhos relativos

--- Exemplos/ex-05-04/ex-assincrono.js
+++ Exemplos/ex-05-04/ex-assincrono.js
@@ -5,17 +5,13 @@ console.log("Iniciando leitura do arquivo...");
 
 // não trava a execução
 // usa callback para notificar quando terminar
-readFile(
-  "/Users/lucas-lfm/Documents/praticas-web1-2026.1/exemplos/ex-05-04/arquivo.txt",
-  "utf-8",
-  (err, dados) => {
-    if (err) {
-      console.error("Erro na leitura:", err);
-      return;
-    }
+readFile(`${import.meta.dirname}/arquivo.txt`, "utf-8", (err, dados) => {
+  if (err) {
+    console.error("Erro na leitura:", err);
+    return;
+  }
 
-    console.log("Leitura concluída:", dados);
-  },
-);
+  console.log("Leitura concluída:", dados);
+});
 
 console.log("Término do código síncrono...");
--- Exemplos/ex-05-04/ex-sincrono.js
+++ Exemplos/ex-05-04/ex-sincrono.js
@@ -1,11 +1,12 @@
 // Síncrono e Bloqueante
 import { readFileSync } from 'node:fs';
+import { dirname } from 'node:path';
 
 console.log("Iniciando leitura do arquivo...");
 
 // trava a execução até ler o arquivo
 const dados = readFileSync(
-  "/Users/lucas-lfm/Documents/praticas-web1-2026.1/exemplos/ex-05-04/arquivo.txt",
+  `${import.meta.dirname}/arquivo.txt`,
   "UTF-8",
 );
 
